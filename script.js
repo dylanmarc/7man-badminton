356b09c277dac12eb3506b027fce67ba8f129ae7
dylanmarc <diloubilou@gmail.com>
2021-05-14 23:38:24 +1200

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+    players = [];
+    rounds = 0;
+
+    //adds players
+    $('#add').click(function () {
+        console.log(players.length)
+        if (players.length < 7) {
+            var newPlayer = $('#playerInput').val();
+            players.push(newPlayer);
+            $('#playerList').append(`<li>${newPlayer}</li>`);
+        } else {
+            alert('too many u buht');
+        }
+    })
+
+    //first 4 on
+    //last 3 of on players off, first off player goes on
+
+
+    $('#matchmake').click(function () {
+        if (players.length == 7) {
+            sevenPlayer();
+        }else{
+            alert('ya buht')
+        }
+    })
+
+    $('#clear').click(function () {
+        players = []
+        $('#playerList').empty()
+        $('#match').empty()
+    })
+
+    function sevenPlayer() {
+        var playersOn = []
+        var playersOff = []
+        for (i = 0; players.length > i; i++) {
+            console.log(i)
+            if (i <= 3) {
+                playersOn.push(players[i]);
+            } else {
+                playersOff.push(players[i]);
+            }
+        }
+        rounds++;
+        $('#match').html(`<b>Round: ${rounds}</b><div class="text-success">${playersOn}</div><div class="text-danger">${playersOff}</div>`)
+        var newPlayers = [];
+        for (i = 0; playersOff.length > i; i++) {
+            newPlayers.push(playersOff[i]);
+        }
+        for (i = 0; playersOn.length > i; i++) {
+            newPlayers.push(playersOn[i]);
+        }
+        players = newPlayers;
+    }
+
+    Array.prototype.move = function (from, to) {
+        this.splice(to, 0, this.splice(from, 1)[0]);
+    };
+
+
+    // $('#count').click(function () {
+    //     countPlayers()
+    // })
+
+    //counts players
+    // function countPlayers() {
+    //     $('#playerList').find('li').each(function () {
+    //         players.push($(this).text());
+    //     });
+    //     console.log(players);
+    //     return players;
+    // }
+})
