7185cebb3e761cd505bff2df5a38db63196eb82c
dylanmarc <diloubilou@gmail.com>
2021-05-15 18:26:04 +1200

    new styling, added default player list, more validation

--- script.js
+++ script.js
@@ -2,40 +2,66 @@ $(document).ready(function () {
 
     players = [];
     rounds = 0;
+    defaultPlayers = [' Dylan', ' Lewis', ' George', ' Ravi', ' Callum', ' Jake', ' Max'];
+
 
     //adds players
     $('#add').click(function () {
-        console.log(players.length)
+
         if (players.length < 7 && $('#playerInput').val() != '') {
-            var newPlayer = $('#playerInput').val();
-            players.push(newPlayer);
-            $('#playerList').append(`<li>${newPlayer}</li>`);
-            $('#playerInput').val('');
-        } else if$('#playerInput').val() != ''){
-            alert('enter someone u buht')
+
+            if ($('#playerInput').val() == 'default' || $('#playerInput').val() == '$') {
+                players = defaultPlayers;
+                $('#playerList').empty();
+                for (i = 0; players.length > i; i++) {
+                    $('#playerList').append(`<li class="list-group-item bg-dark text-white border-secondary">${players[i]}</li>`);
+                }
+                $('#divider').removeClass('d-none');
+
+            } else {
+                var newPlayer = $('#playerInput').val();
+                players.push(newPlayer);
+                $('#playerList').append(`<li class="list-group-item bg-dark text-white border-secondary">${newPlayer}</li>`);
+                $('#playerInput').val('');
+
+                $('#divider').removeClass('d-none');
+            }
+
+        } else if ($('#playerInput').val() == '') {
+            alert('enter someone u no-life cretin')
         } else {
-            alert('too many u buht');
+            alert('too many ppl u fucking idiot');
         }
     })
 
-    //first 4 on
-    //last 3 of on players off, first off player goes on
-
-
+    //makes a match if you have seven players
     $('#matchmake').click(function () {
         if (players.length == 7) {
             sevenPlayer();
-        }else{
-            alert('ya buht')
+        } else {
+            alert('Go fuck yourself (not enough people)')
         }
     })
 
+    //clears player list and matchups
     $('#clear').click(function () {
         players = []
         $('#playerList').empty()
         $('#match').empty()
+        $('#divider').addClass('d-none');
     })
 
+    //makes it so you can press enter to add players to the list
+    $('#playerInput').keyup(function () {
+        if (event.keyCode === 13) {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            $('#add').click();
+        }
+    })
+
+    //takes array of players and calculates who is on and off
     function sevenPlayer() {
         var playersOn = []
         var playersOff = []
@@ -48,7 +74,7 @@ $(document).ready(function () {
             }
         }
         rounds++;
-        $('#match').html(`<b>Round: ${rounds}</b><div class="text-success">${playersOn}</div><div class="text-danger">${playersOff}</div>`)
+        $('#match').html(`<b class="text-white bg-dark list-group-item bg-dark text-white border-secondary">Round: ${rounds}</b><div class="text-white border-secondary bg-success list-group-item">${playersOn}</div><div class="text-white border-secondary bg-danger list-group-item">${playersOff}</div>`)
         var newPlayers = [];
         for (i = 0; playersOff.length > i; i++) {
             newPlayers.push(playersOff[i]);
@@ -58,22 +84,8 @@ $(document).ready(function () {
         }
         players = newPlayers;
     }
-
     Array.prototype.move = function (from, to) {
         this.splice(to, 0, this.splice(from, 1)[0]);
     };
 
-
-    // $('#count').click(function () {
-    //     countPlayers()
-    // })
-
-    //counts players
-    // function countPlayers() {
-    //     $('#playerList').find('li').each(function () {
-    //         players.push($(this).text());
-    //     });
-    //     console.log(players);
-    //     return players;
-    // }
 })
